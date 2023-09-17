c6f99e6bb985de57b14ff1ef6d346e6dbe26acee
JULIUS <JULIUS@OGUNNIYIs-MacBook-Pro.local>
2023-09-17 18:55:02 +0100

    login

--- src/component/Register/StudentLogin.jsx
+++ src/component/Register/StudentLogin.jsx
@@ -327,7 +327,7 @@ const StudentLogin = () => {
                   <input
                     type={showPassword ? "text" : "password"} 
                     className="border border-gray-300 rounded-md py-2 px-3 text-sm text-gray-700 focus:outline-none focus:border-blue-500 placeholder:text-sm"
-                    {...reactHookFormRegRegister("password")}
+                    {...reactHookFormRegister("password")}
                     placeholder="Password"
                   />
                   <div className="showPassword">
