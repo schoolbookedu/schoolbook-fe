6d258af9854b83f9901d5af4b3758bef99d1473c
Julius <ogunniyi.julius12@gmail.com>
2023-12-05 15:29:26 +0100

    feat:modified-ui

--- src/component/Featured Courses/Created.jsx
+++ src/component/Featured Courses/Created.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Created = ({ resource }) => {
   const alt = `${resource?.title} by ${resource?.tutor?.fullName}`;
   return (
-    <div className="fcourse-created">
+    <div className="fcourse">
       <div className="fcourse-img">
         <img src={resource?.thumbnail} alt={alt} />
       </div>
