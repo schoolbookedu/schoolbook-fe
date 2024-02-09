49e90a133280e8a244f6ef6dbcf177bf07eeaabf
ikechukwu-peter <pitiyke@gmail.com>
2024-02-09 08:12:31 +0100

    fix: module route

--- src/loaders/overlay.js
+++ src/loaders/overlay.js
@@ -1,7 +1,7 @@
 export const Overlay = (props) => {
   if (props.show) {
     return (
-      <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-60 backdrop-filter backdrop-blur-[2px]   z-50 pointer-events-none">
+      <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-60 backdrop-filter backdrop-blur-[2px]   z-[999999] pointer-events-none">
         {props.children}
       </div>
     );
